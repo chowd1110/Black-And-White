4b6c4ed80b2b397c8a0880b09b88633a08fd3282
Saadaf Chowdhury <saadaf.chowdhury@mail.utoronto.ca>
2019-07-15 16:44:33 -0400

    Complete main() method in View

--- src/View.java
+++ src/View.java
@@ -1,9 +1,27 @@
+import java.util.Observable;
+import java.util.Observer;
 
-public class View {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class View extends Application implements Observer{
 
 	public static void main(String[] args) {
-		
+		launch(args);
 
+	}
+	
+
+	@Override
+	public void update(Observable o, Object arg) {
+		
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		
 	}
 
 }
