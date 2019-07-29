c8b2389236879a0dd13de48c1723365b9c8b672d
Saadaf Chowdhury <saadaf.chowdhury@mail.utoronto.ca>
2019-07-29 19:33:49 -0400

    Add constructor for CheckerBoard
    If a CheckerBoard object is initiated, an 8x8 array is created which
    contains the strings "Empty", "Black" and "White which represents the
    position of each checker in the current game.

--- src/checkers/CheckerBoard.java
+++ src/checkers/CheckerBoard.java
@@ -1,5 +1,44 @@
 package checkers;
 
 public class CheckerBoard {
-
+	private String[][] board = new String[8][8];
+	
+	public CheckerBoard() {
+		initEmptyBoard(); 
+		
+		int[][] blackCheckerPos = {{0,1},{1,0},{1,2},{2,1},{3,0},
+				{3,2},{4,1},{5,0},{5,2},{6,1},{7,0},{7,2}};
+		
+		addCheckers(blackCheckerPos, "Black");
+		
+		int[][] whiteCheckerPos = {{0,5},{0,7},{1,6},{2,5},{2,7},{3,6},{4,5},
+				{4,7},{5,6},{6,5},{6,7},{7,6}};
+		
+		addCheckers(whiteCheckerPos, "White");
+		
+	}
+    
+	private void initEmptyBoard() {
+		
+		for (int i = 0; i < this.board.length; i++ ) {
+			for (int j = 0; j < this.board.length; j++) {
+				this.board[i][j] = "Empty"; //  empty = that position on the board does not
+				                       // contain a checker
+			}
+		}
+	}
+	
+	private void addCheckers(int[][] positions, String Colour) {
+		int column;
+		int row;
+		
+		for (int i = 0; i < positions.length; i++) {
+			column = positions[i][0];
+			row = positions[i][1];
+			this.board[column][row] = Colour;
+			
+		}
+	}
+	
+	
 }
