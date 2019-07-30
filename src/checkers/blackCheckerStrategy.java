b64b025f28eec7de15d80641f062b81af6a042a3
Saadaf Chowdhury <saadaf.chowdhury@mail.utoronto.ca>
2019-07-30 13:12:55 -0400

    Add constructor for blackCheckerStrategy

--- src/checkers/blackCheckerStrategy.java
+++ src/checkers/blackCheckerStrategy.java
@@ -1,6 +1,13 @@
 package checkers;
 
 public class blackCheckerStrategy {
+	private CheckerBoard model;
+	
+	public blackCheckerStrategy(CheckerBoard board) {
+		this.model = board;
+	}
+	
+	
 	
 
 }
