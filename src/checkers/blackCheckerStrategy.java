2bc54c1d2df6275f02af5e20392ede7d01102dd1
Saadaf Chowdhury <saadaf.chowdhury@mail.utoronto.ca>
2019-08-01 22:11:30 -0400

    Modify moveLeft() in blackCheckerStrategy
    moveLeft() now accounts for all 3 cases when a checker is moved left.

--- src/checkers/blackCheckerStrategy.java
+++ src/checkers/blackCheckerStrategy.java
@@ -10,9 +10,32 @@ public class blackCheckerStrategy {
 	public void moveLeft(Checker checker) {
 		int[] currentPosition = checker.getPosition();
 		int[] newPosition =  {currentPosition[0] - 1 , currentPosition[1] + 1}; 
+		// check if newPosition is inside the board here
+		String checkerAtNewPosition = this.model.getCheckerAtPosition(newPosition);
 		
-		checker.setPosition(newPosition);
-		this.model.updateBoard(checker, currentPosition);
+		
+		if(checkerAtNewPosition == checker.getColour()) { // there is already a black checker at newPosition
+			// window that says you cannot do that
+		}
+		
+		else {
+			
+			
+			if (checkerAtNewPosition == "Empty") { // there is neither a white or black checker at newPosition
+				checker.setPosition(newPosition);
+				this.model.updateBoard(checker, currentPosition);
+			}
+			else { // there is a white checker at newPosition. Make a Jump
+				int[] checkerPositionAfterJumping = {newPosition[0] - 1 , newPosition[1] + 1};
+				checker.setPosition(checkerPositionAfterJumping);
+				checker.setJumped(true);
+				this.model.updateBoard(checker, currentPosition, newPosition);
+			}
+			
+				
+			
+			
+		}
 		
 	}
 	
