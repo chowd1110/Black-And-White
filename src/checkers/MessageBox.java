fff5cf3454a9bd8a805cb5cbd12c2b4d0efe7d5c
Saadaf Chowdhury <saadaf.chowdhury@mail.utoronto.ca>
2019-08-05 19:16:55 -0400

    Add createMessageBox() function to MessageBox

--- src/checkers/MessageBox.java
+++ src/checkers/MessageBox.java
@@ -1,5 +1,37 @@
 package checkers;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 public class MessageBox {
+	
+	public static void createMessageBox(String message, String title) {
+		Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle(title);
+		
+		window.setMinWidth(300);
+		window.setMinHeight(100);
+		
+		Label label = new Label();
+		label.setText(message);
+		
+		VBox layout = new VBox(10);
+		layout.getChildren().add(label);
+		
+		layout.setAlignment(Pos.CENTER);
+		
+		Scene scene = new Scene(layout);
+		
+		window.setScene(scene);
+		window.showAndWait();
+		
+	
+		
+	}
 
 }
